1510bca421062067f6d3c6cf3f26563ea2bf5d07
Kobistechmd <ooaimyf735@gmail.com>
2024-09-22 15:49:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.ownernomer = "48459080556"
+global.ownername = "john mark"
+global.ytname = "YT: peteroghenekobiruo"
+global.socialm = "GitHub: kobistechmd"
+global.location = "Africa, Nigeria, Delta"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '48459080556'  //creator number
+global.ownername = 'john mark' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: 48459080556'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029Vabb7YwFSAt31QUidn1k'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
